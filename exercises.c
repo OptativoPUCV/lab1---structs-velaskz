bcaa22af33e05d168decb8f8fc86bce86711c8d0
Replit user <>
2023-08-16 14:08:08 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -163,6 +163,7 @@ void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
   strcpy(libro-> titulo, titulo);
   strcpy(libro->autor.nombre, nombreAutor);
   libro->autor.anioNacimiento = anioNacimiento;
+  libro->anioPublicacion = anioPublicacion;
 
 }
 /*
